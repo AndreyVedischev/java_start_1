6fec4a2004c2be68e79f6623fda12dda06657d93
Andrey <vedischev76@gmail.com>
2022-10-20 14:43:06 +0500

    update FirstTask

--- src/main/java/string_array/FirstTask.java
+++ src/main/java/string_array/FirstTask.java
@@ -1,7 +1,14 @@
 package string_array;
-
+import java.util.Arrays;
 public class FirstTask {
     public static void main(String[] args) {
-        System.out.println("Решение задачи 1");
+        System.out.println("Решение задачи 1920");
+
+        int[] nums = {0,2,1,5,3,4};
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length;i++) {
+            ans[i] = nums[nums[i]];
+        }
+        System.out.println(Arrays.toString(ans));
     }
 }
