05302f8c32616b3e2e5a323487a770cdf150c664
Andrey <vedischev76@gmail.com>
2022-10-20 15:11:48 +0500

    update SecondTask

--- src/main/java/string_array/SecondTask.java
+++ src/main/java/string_array/SecondTask.java
@@ -1,7 +1,22 @@
 package string_array;
+import java.util.Arrays;
 
 public class SecondTask {
     public static void main(String[] args) {
-        System.out.println("Решение задачи 2");
+        System.out.println("Решение задачи 283");
+
+        int[] num= {0,1,2,2,3,0,4,2};
+        int val = 2;
+        int copyIndex = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] != val) {
+                num[copyIndex] = num[i];
+                copyIndex++;
+            }
+        }
+        while (copyIndex < num.length) {
+            num[copyIndex++] = 0;
+        }
+        System.out.println(Arrays.toString(num));
     }
 }
