9f76b08a72935eab82d380f2f3da231e303ade56
Andrey <vedischev76@gmail.com>
2022-10-24 16:52:34 +0500

    update+ Task231

--- src/main/java/lesson2/recursiya/Task231.java
+++ src/main/java/lesson2/recursiya/Task231.java
@@ -2,6 +2,7 @@ package lesson2.recursiya;
 
 public class Task231 {
     public static void main(String[] args) {
+        System.out.println("Решение задачи 231: ");
         System.out.println(isPowerOfTwo(16));
     }
     public static boolean isPowerOfTwo(int n) {
