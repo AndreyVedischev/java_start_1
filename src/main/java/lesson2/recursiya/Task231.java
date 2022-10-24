6fdadb1524007104c15f08db2f629fada14850a2
Andrey <vedischev76@gmail.com>
2022-10-24 14:30:46 +0500

    update Task231 submit

---
+++ src/main/java/lesson2/recursiya/Task231.java
@@ -0,0 +1,14 @@
+package lesson2.recursiya;
+
+public class Task231 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
+    public static boolean isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        if (n % 2 != 0) return false;
+        if (n == 0) return false;
+        return isPowerOfTwo(n/2);
+
+    }
+}
